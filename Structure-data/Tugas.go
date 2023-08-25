bb9f1dea35279238de04c89995d0de4c242bc04a
aufall02 <aufaluzumaki@gmail.com>
2023-08-25 19:53:38 +0700

    Optimasi Array merge

--- Structure-data/Tugas.go
+++ Structure-data/Tugas.go
@@ -5,35 +5,60 @@ import (
 )
 
 func main() {
-	fmt.Println("soal pertama  : " ,arrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie","steve", "geese"}));
-	fmt.Println("soal kedua    : " ,arrayMerge([]string{"sergei", "jin"}, []string{"jin","steve", "bryan"}));
-	fmt.Println("soal ketiga   : " ,arrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin","yoshimitsu", "alisa", "law"}));
-	fmt.Println("soal keempat  : " ,arrayMerge([]string{}, []string{"devil jin","sergei"}));
-	fmt.Println("soal kelima   : " ,arrayMerge([]string{"hwoarang"}, []string{}));
-	fmt.Println("soal keenam   : " ,arrayMerge([]string{}, []string{}));
+	fmt.Println("soal pertama : ", arrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
+	fmt.Println("soal kedua   : ", arrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
+	fmt.Println("soal ketiga  : ", arrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
+	fmt.Println("soal keempat : ", arrayMerge([]string{}, []string{"devil jin", "sergei"}))
+	fmt.Println("soal kelima  : ", arrayMerge([]string{"hwoarang"}, []string{}))
+	fmt.Println("soal keenam  : ", arrayMerge([]string{}, []string{}))
+
+	fmt.Println("====================================================================")
+	fmt.Println("soal pertama : ", Mapping([]string{"asd", "qwe", "asd", "adi", "qwe", "qwe"}))
+	fmt.Println("soal kedua   : ", Mapping([]string{"asd", "qwe", "asd"}))
+	fmt.Println("soal ketiga  : ", Mapping([]string{}))
 }
 
 func arrayMerge(arrayA, arrayB []string) []string {
-	mergeArray := make([]string, len(arrayA)+len(arrayB))
+	
+	var mergeArray []string
+	var NewArray []string
+	mapChecker := make(map[string]int)
+
+
 	//merge array
 	mergeArray = append(arrayA, arrayB...)
 
 	//cek apakah ada niali yang sama
-	//membuat map 
-	mapChecker := make(map[string]int)
+	//membuat map
+	
 
 	// lakukan perulangan untuk memasukkan nilai array yang sudah di merge ke dalam map
 	// karna key di map bersifat unik maka value di array diganti menjadi key di map agar otomatis menghapus value uang sama
-	for i, v := range mergeArray{
-		mapChecker[v] = i
+	for i, v := range mergeArray {
+		_, exist := mapChecker[v]
+		if !exist {
+			mapChecker[v] = i
+			NewArray = append(NewArray, v)
+		}
 	}
 
-	//setelah melakukan map ubah kembali key dari menjadi value di array baru
-	//mebuat array baru
-	newarray := make([]string, len(mergeArray))
-	for key, values := range mapChecker{
-		newarray[values] = key
+	return NewArray
+}
+
+func Mapping(slice []string) map[string]int {
+	result := make(map[string]int)
+
+
+	for _, v := range slice {
+		value, exist := result[v]
+		if exist {
+			result[v] = value+1
+		}else{
+			result[v] = 1
+		}
+		
 	}
 
-	return newarray
+	return result
+
 }
